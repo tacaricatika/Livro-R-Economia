4f8115c5f2ae7bf394eb430c849419902c8540a3
tacaricatika <73254030+tacaricatika@users.noreply.github.com>
2021-01-04 20:24:24 -0300

    Comitando

--- codigo.R
+++ codigo.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 
+#CRIANDO COLUNAS, ELEMENTOS E UM DATAFRAME
 Runas <- rep(c('conquistador', 'pressione', 'ritmo fatal'), 4)
 Runas
 
@@ -12,4 +13,24 @@ Dia
 Abates <- c(12, 08, 14, 12, 08, 10, 06, 08, 09, 10, 11, 11)
 
 LOL <- tibble(Runas, Dia, Abates)
+View(LOL)
+
+# See and Change column names
+colnames(LOL)
+print(LOL[1:3, c('Runes', 'Day')])
+LOL <- LOL %>% 
+  rename(Runes=Runas) %>% 
+  rename(Day=Dia) %>% 
+  rename(Kills=Abates)
+
+#create a new column
+LOL <- LOL %>% 
+  mutate(Assist= c(15, 14, 08, 15, 14, 17, 18, 14, 12, 13, 11, 12),
+         Death=c(04, 08, 04, 03, 01, 03, 02, 05, 07, 02, 03, 03),
+          AMA=Kills+Assist/Death) %>% 
+  glimpse()
+
+#Delete a column
+LOL <- LOL %>% 
+  select("Runes", "Day", "Kills",  "Assist", "Death", "AMA")
 View(LOL)
